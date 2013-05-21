df09dc8ccd8e381b06927d280f30675464d87836
rongzheyi <rongzheyi@gmail.com>
2013-05-21 14:20:29 +0200

    modified fp-synthetic

--- experiments/fpg/synthetic/fpg-support.sh
+++ experiments/fpg/synthetic/fpg-support.sh
@@ -3,7 +3,7 @@
 SUPPORT=$1	
 
 mahout fpg	\
-	-D mapred.reduce.tasks=1	\
+	-Dmapred.reduce.tasks=1	\
 	-i /user/s117449/synthetic/d_100m_20_1k_10k_4.data	\
 	-o output/fpg-synthetic-support${SUPPORT}	\
 	-method mapreduce	\
