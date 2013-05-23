4c3b86def5c8e0cec7d3f15c077c3c220b68e183
rongzheyi <rongzheyi@gmail.com>
2013-05-23 15:25:05 +0200

    moved count_pattern.py

--- experiments/count_patterns.py
+++ experiments/count_patterns.py
@@ -10,12 +10,14 @@ import operator
 count = {}
 total = 0
 for line in sys.stdin:
-	line = line.rstrip()
+	line = line.strip().split('\t', 1)[1]
+	pattern = line.split(' ')
+	line = ' '.join(sorted(pattern))
 	count[line] = count.get(line, 0) + 1
 	total += 1
 result = sorted(count.iteritems(), key=operator.itemgetter(1), reverse=True)
 
-print "Pattern\tCount\tPercentage"
+print "Percent\tCount\t|Pattern"
 for line in result:
-	print line[0] + "\t" + str(line[1]) + "\t" +str(line[1]*1.0/total)
-print "Total\t" + str(total) + "\t1"
+	print str(line[1]*1.0/total)  + "\t" + str(line[1]) + "\t|" + line[0] 
+print "1\t" + str(total)
